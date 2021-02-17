92d46d40c1532e4cc131fd178bb97e7beb971112
michaelhumphrey <michaelhumphrey@mymail.mines.edu>
2021-02-17 10:40:42 -0700

    P4 COmplete

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -10,3 +10,46 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+
+//
+//    ALGORITHM:
+//        R2 = 0
+//        while (R0 > 0)
+//            R2 += R1
+//            R0--
+//
+
+// This block initializes R2 to zero so that the sum starts correctly
+@0
+D=A
+@R2
+M=D
+
+// This is the main loop
+//  Checks whether or not R0 is still positive
+//    Ends if not
+//  Increments R2=R2+R1
+//  Decrements R0
+(LOOP)
+    @R0             
+    D=M
+    @END
+    D;JLE
+
+
+    @R1
+    D=M
+    @R2
+    M=M+D
+
+    @R0 
+    M=M-1
+    D=M
+
+    @LOOP
+    0;JMP
+
+// This block ends the program
+(END)
+    @END
+    0;JMP
